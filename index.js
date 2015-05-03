e947c83aa049f9b7054116ae83ef347d12dbcc53
Tommy Bergeron <tom@brainpad.org>
2015-05-03 21:33:41 +0000

    yep

---
+++ index.js
@@ -0,0 +1,13 @@
+var settings = {
+    invested_markets : [
+        {
+            'market_name' : 'BTC-UTC',
+            'my_bid' : 0.00001100
+        },
+        {
+            'market_name' : 'BTC-EKN',
+            'my_bid' : 0.00004600
+        },
+    ]
+};
+

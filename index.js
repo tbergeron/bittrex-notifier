15be65f3c68109615821391f9dab8c86ba8da0fb
Tommy Bergeron <tom@brainpad.org>
2015-05-03 22:27:36 +0000

    criss

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var settings = {
-    delay : 5, // minutes
-    verbose: true,
+    delay : 1, // minutes
+    verbose: false,
     warning_threshold : 1.25, // 1.%%
     invested_markets : [
         {
@@ -20,21 +20,35 @@ var host = 'http://bittrex.brainpad.org',
     markets_api_uri = '/api/get_markets',
     market_summary_api_uri = '/api/get_market_summary/';
 
-// todo: every /delay/ minutes execute the loop
+// every /delay/ minutes execute the loop
+setInterval(function() {
+    checkUpdates();
+}, calculateDelay());
 
-settings.invested_markets.forEach(function(market) {
-    getCurrentMarketDetails(market.market_name, function(details) {
-        v('Fetching ' + market.market_name + '...');
-        // calculate change percentage
-        var diff = details.Last / market.my_bid;
-        v('Difference (' + details.Last.toFixed(8) + '/' + market.my_bid.toFixed(8) + ') = ' + diff + '%');
-        // if percentage is higher than warning threshold, notify
-        if (diff > settings.warning_threshold) {
-            v('Diff (' + diff + ') is over threshold (' + settings.warning_threshold + ')!');
-            console.log('[*] You should check out ' + market.market_name + '!');
-        }
+// intial run
+checkUpdates();
+
+function checkUpdates() {
+        settings.invested_markets.forEach(function(market) {
+        getCurrentMarketDetails(market.market_name, function(details) {
+            v('Fetching ' + market.market_name + '...');
+            // calculate change percentage
+            var diff = details.Last / market.my_bid;
+            v('Difference (' + details.Last.toFixed(8) + '/' + market.my_bid.toFixed(8) + ') = ' + diff + '%');
+            // if percentage is higher than warning threshold, notify
+            if (diff > settings.warning_threshold) {
+                v('Diff (' + diff + ') is over threshold (' + settings.warning_threshold + ')!');
+                console.log('[*] You should check out ' + market.market_name + '! ' + now());
+            }
+        });
     });
-});
+}
+
+function calculateDelay() {
+    var calulated_delay = settings.delay * 60 * 1000;
+    v('Delay is set to run every ' + settings.delay + ' min(s) (' + calulated_delay + ' ms).');
+    return calulated_delay;
+}
 
 function getCurrentMarketDetails(market_name, done) {
     request(host + market_summary_api_uri + market_name, function (error, response, body) {
@@ -47,6 +61,10 @@ function getCurrentMarketDetails(market_name, done) {
             }
         }
     });
+}
+
+function now() {
+    return new Date().toLocaleString();
 }
 
 function v(msg) {
